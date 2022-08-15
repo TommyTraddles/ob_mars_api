1a0b079f4e4736b703f6196ee64edc669b7deae6
Tommy Traddles <jandresnova@gmail.com>
2022-08-16 00:03:59 +0200

    chore: updating route to delegate incoming queries

--- src/api/index.js
+++ src/api/index.js
@@ -2,12 +2,7 @@ const router = require('express').Router()
 const validator = require('../middlewares/validate-input')
 
 module.exports = (db) => {
-  router.post('/', validator, (req, res, next) => {
-    res.json({
-      success: true,
-      message: 'hello',
-    })
-  })
+  router.post('/', validator, require('./post-mission')(db))
 
   return router
 }
