079e4bea2e11711aaa206ccbf2f201737f2affcc
Tommy Traddles <jandresnova@gmail.com>
2022-08-16 00:07:19 +0200

    chore: defining the statistics needed queries

---
+++ src/api/post-mission/queries/mission-statistics-queries.js
@@ -0,0 +1,54 @@
+const { sql } = require('slonik')
+
+async function retrieveMissionExploredSurface(db, { BOARD }) {
+  return await db.query(sql`
+    SELECT
+      DISTINCT x, y
+    FROM robots_logs 
+    WHERE 
+      surface_id = ${BOARD.id}
+      AND x <= ${BOARD.x}
+      AND y <= ${BOARD.y}
+      ORDER BY x;
+  `)
+}
+
+async function retrieveIdentifiedDangerZones(db, { BOARD }) {
+  return await db.query(sql`
+    SELECT x, y, compass
+    FROM danger_zones
+    WHERE surface_id = ${BOARD.id};
+  `)
+}
+
+async function retrieveJourneyByRobot(db, { id }, { BOARD }) {
+  return await db.query(sql`
+    SELECT
+      step, x, y, lost_signal, compass, instruction
+    FROM robots_logs 
+    WHERE 
+      surface_id = ${BOARD.id}
+      AND robot_id = ${id}
+      ORDER BY step;
+  `)
+}
+
+async function retrieveUniqueSurfacesByRobot(db, { id }, { BOARD }) {
+  return await db.query(sql`
+    SELECT
+      DISTINCT x, y
+    FROM 
+      robots_logs 
+    WHERE 
+      surface_id = ${BOARD.id}
+      AND robot_id = ${id}
+      AND lost_signal = false;
+    `)
+}
+
+module.exports = {
+  retrieveJourneyByRobot,
+  retrieveIdentifiedDangerZones,
+  retrieveUniqueSurfacesByRobot,
+  retrieveMissionExploredSurface,
+}
